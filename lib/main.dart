3e4dfa8338148f3e54c3709253ea63c6ef76d3fb
Tan dev <tandev151>
2024-02-09 16:53:11 +0700

    handle done my card

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,12 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.teal,
           body: SafeArea(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
+                // SizedBox(
+                //   width: double.infinity,
+                // ),
                 CircleAvatar(
                   radius: 50,
                   backgroundImage: AssetImage('images/avatar.jpg'),
@@ -24,7 +29,58 @@ class MyApp extends StatelessWidget {
                     style: TextStyle(
                         fontFamily: 'Pacifico',
                         fontWeight: FontWeight.bold,
-                        color: Colors.white))
+                        fontSize: 40.0,
+                        color: Colors.white)),
+                Text('Web developer'.toUpperCase(),
+                    style: TextStyle(
+                        fontFamily: 'SourceSan3',
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.teal.shade100,
+                        letterSpacing: 2.25)),
+                SizedBox(
+                  height: 30.0,
+                  width: 150.0,
+                  child: Divider(color: Colors.teal[100]),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      size: 30.0,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+83 379 79 38 15',
+                      style: TextStyle(
+                          fontFamily: 'SourceSan3',
+                          color: Colors.teal,
+                          fontSize: 20.0),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+                  child: ListTile(
+                    title: Text(
+                      'tanleit151@gmail.com',
+                      style: TextStyle(
+                          fontFamily: 'SourceSan3',
+                          color: Colors.teal,
+                          fontSize: 20.0),
+                    ),
+                    leading: Icon(
+                      Icons.email,
+                      size: 30.0,
+                      color: Colors.teal,
+                    ),
+                  ),
+                )
               ],
             ),
           )),
